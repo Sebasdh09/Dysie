b37a28f8bdf6bc849c1b928a29813437c754ad34
Sebasdh09 <sebas200420@gmail.com>
2023-11-22 00:22:06 -0500

    todo funciona

--- inicio/index.php
+++ inicio/index.php
@@ -313,7 +313,7 @@ color:white;
             <input type="hidden" id="editTarjetaId" name="id_tarjetas">
             <label for="editTarjetaNombre">Nuevo Nombre:</label>
             <input type="text" id="editTarjetaNombre" name="nombre_tarjeta" required>
-            <button type="button" onclick="guardarCambiosTarjeta()">Guardar Cambios</button>
+            <button type="submit" onclick="guardarCambiosTarjeta()">Guardar Cambios</button>
         </form>
     </div>
 </div>
@@ -339,33 +339,8 @@ function cerrarEditor() {
     document.getElementById('modalEditar').style.display = 'none';
 }
 
-function guardarCambios() {
-    var form = document.getElementById('formEditarTablero');
-    var formData = new FormData(form);
-
-    // Realiza una solicitud AJAX para enviar los datos al servidor PHP
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "../tableros/editar_tablero.php", true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // Analiza la respuesta JSON del servidor
-            var response = JSON.parse(xhr.responseText);
-            if (response.success) {
-                // Actualiza el color en tiempo real en el tablero
-                var idTablero = document.getElementById('editTableroId').value;
-                var tableroTitle = document.getElementById('tablero-title-' + idTablero);
-                tableroTitle.style.backgroundColor = response.color;
-            } else {
-                console.error("Error al guardar cambios: " + response.error);
-            }
-
-            // Cierra el formulario emergente
-            cerrarEditor();
-        }
-    };
-    xhr.send(formData);
-}
 </script>
+<!-- Aqui esta el de las tarjetas -->
 <script>
     function mostrarEditorTarjeta(idTarjeta, nombreTarjeta) {
         document.getElementById('editTarjetaId').value = idTarjeta;
@@ -377,35 +352,6 @@ function guardarCambios() {
     function cerrarEditorTarjeta() {
         document.getElementById('modalEditarTarjeta').style.display = 'none';
     }
-
-    function guardarCambiosTarjeta() {
-        var form = document.getElementById('formEditarTarjeta');
-        var formData = new FormData(form);
-
-        // Realiza una solicitud AJAX para enviar los datos al servidor PHP
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "../tarjetas/editar_tarjeta.php", true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                // Analiza la respuesta JSON del servidor
-                var response = JSON.parse(xhr.responseText);
-                if (response.success) {
-                    // Actualiza el nombre en tiempo real en la tarjeta
-                    var idTarjeta = document.getElementById('editTarjetaId').value;
-                    var tarjetaNombre = document.querySelector('.tarjeta-nombre[data-id="' + idTarjeta + '"]');
-                    tarjetaNombre.innerHTML = response.nombre;
-                } else {
-                    console.error("Error al guardar cambios: " + response.error);
-                }
-
-                // Cierra el formulario emergente
-                cerrarEditorTarjeta();
-            }
-        };
-        xhr.send(formData);
-    }
-</script>
-<!-- Aqui esta el de las tarjetas -->
 </script>
 <!-- Scripts al final del documento -->
 <script>
--- tarjetas/editar_tarjeta.php
+++ tarjetas/editar_tarjeta.php
@@ -1,5 +1,4 @@
 <?php
-
 // Verifica si se ha enviado el formulario de edición
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Conexión a la base de datos (asegúrate de tener tus propias credenciales)
@@ -19,8 +18,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $idTarjeta = $_POST['id_tarjetas'];
     $nuevoNombre = $_POST['nombre_tarjeta'];
 
+    // Agrega mensajes de depuración
+    error_log("Inicio de la actualización de tarjeta");
+
     // Actualiza el nombre de la tarjeta en la base de datos
-    $sql = "UPDATE tarjeta SET nom_tarjeta = '$nuevoNombre' WHERE id_tarjetas = '$idTarjeta'";
+    $sql = "UPDATE tarjeta SET `nom_tarjeta` = '$nuevoNombre' WHERE `id_tarjetas` = $idTarjeta";
 
     if ($conn->query($sql) === TRUE) {
         // Redirige a la página principal después de editar la tarjeta (ajusta la URL según tu estructura)
@@ -28,8 +30,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     } else {
         echo "Error al editar la tarjeta: " . $conn->error;
+        // o error_log("Error al editar la tarjeta: " . $conn->error);
     }
 
+    // Agrega mensajes de depuración
+    error_log("Fin de la actualización de tarjeta");
+    
     $conn->close();
 }
 ?>
